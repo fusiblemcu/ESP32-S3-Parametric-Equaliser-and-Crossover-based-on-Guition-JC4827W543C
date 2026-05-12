df246f7a1060dcd660b4f140006b8c33f638e031
fusiblemcu <adfoxer@gmail.com>
2026-05-12 20:10:26 +1100

    Clean up comments in ParaEQ_UI_Demo.ino
    Removed unnecessary comments and configuration details.

--- ParaEQ_UI_Demo.ino
+++ ParaEQ_UI_Demo.ino
@@ -1,20 +1,3 @@
-//* USB CDC On Boot: "Enabled"
-//* CPU Frequency: "240MHz (WiFi)"
-//* Core Debug Level: "Info"
-//* USB DFU On Boot: "Disabled"
-//* Erase All Flash Before Sketch Upload: "Disabled"
-//* Events Run On: "Core 1"
-//* Flash Mode: "QIO 80MHz"
-//* Flash Size: "4MB (32Mb)"
-//* JTAG Adapter: "Disabled"
-//* Arduino Runs On: "Core 1"
-//* USB Firmware MSC On Boot: "Disabled"
-//* Partition Scheme: "Huge APP (3MB No OTA/1MB SPIFFS)"
-//* PSRAM: "OPI PSRAM"
-//* Upload Mode: "UART0 / Hardware CDC"
-//* Upload Speed: "921600"
-//* USB Mode: "Hardware CDC and JTAG"
-//* Zigbee Mode: "Disabled"
 // Board: "ESP32S3 Dev Module" from esp32 Arduino Core by Espressif (v3.2.0)
 // Libraries required:
 //   - lvgl v9.2.2                (Library Manager)
